85a5bf45a6b9673095f8b08360261d854cb143aa
Shantanu Sharma <60518426+Avenger-py@users.noreply.github.com>
2022-08-18 13:53:55 +0530

    for ncnn
    Signed-off-by: Shantanu Sharma <60518426+Avenger-py@users.noreply.github.com>

--- models/common.py
+++ models/common.py
@@ -224,15 +224,15 @@ class SPPF(nn.Module):
 
 
 class Focus(nn.Module):
-    # Focus wh information into c-space
-    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
-        super().__init__()
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):
+        super(Focus, self).__init__()
         self.conv = Conv(c1 * 4, c2, k, s, p, g, act)
-        # self.contract = Contract(gain=2)
 
-    def forward(self, x):  # x(b,c,w,h) -> y(b,4c,w/2,h/2)
-        return self.conv(torch.cat((x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]), 1))
-        # return self.conv(self.contract(x))
+    def forward(self, x):
+        return self.conv(torch.cat([torch.nn.functional.interpolate(x, scale_factor=0.5),
+                                    torch.nn.functional.interpolate(x, scale_factor=0.5),
+                                    torch.nn.functional.interpolate(x, scale_factor=0.5),
+                                    torch.nn.functional.interpolate(x, scale_factor=0.5)], 1))
 
 
 class GhostConv(nn.Module):
